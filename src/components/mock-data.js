127cd129635da355c22d0c7459c2790186a75133
raulpellizzer <raul1.pellizzer1@gmail.com>
2023-02-21 16:02:49 -0300

    Create home page grid template

---
+++ src/components/mock-data.js
@@ -0,0 +1,40 @@
+function MockData() {
+    let data = [
+        {
+            "name": "Expense 01",
+            "description": "My first expense",
+            "category": "House goods",
+            "date": "2022-12-24",
+            "place": "Convenience Store",
+            "amount": 35.50,
+        },
+        {
+            "name": "Expense 02",
+            "description": "Second expense",
+            "category": "House goods",
+            "date": "2022-12-28",
+            "place": "Supermarket",
+            "amount": 78.20,
+        },
+        {
+            "name": "Expense 03",
+            "description": "Yet another expense",
+            "category": "Tech Equipment",
+            "date": "2023-01-02",
+            "place": "Online",
+            "amount": 1005.50,
+        },
+        {
+            "name": "Last Expense",
+            "description": "Latest",
+            "category": "Tech Equipment",
+            "date": "2023-02-21",
+            "place": "Online",
+            "amount": 500.50,
+        }
+    ]
+    
+    return (data);
+}
+
+export default MockData;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,62 @@
 import NavBar from "../components/Navbar";
+import MockData from "../components/mock-data";
 
 function Home() {
     return (
         <>
             <NavBar></NavBar>
-            <h4>Home Page</h4>
-        </>
 
+            <div class="container p-5">
+                <div class="row">
+                    <div class="col-sm-7">
+                        <LastExpenseGrid></LastExpenseGrid>
+                    </div>
+                    <div class="col-sm-2"></div>
+                    <div class="col-sm-3">
+                        Graph will appear here
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+function LastExpenseGrid() {
+    let mockData = MockData(); // Get JSON from API (MOCKED SERVICE ATM)
+    let tableContent = [];
+
+    mockData.forEach((item)=>{ // For each expense, create the row structure and push it into array.
+        tableContent.push(
+        <tr>
+            <td>{item.name}</td>
+            <td>{item.description}</td>
+            <td>{item.category}</td>
+            <td>{item.date}</td>
+            <td>{item.place}</td>
+            <td>{item.amount}</td>
+        </tr>);
+    });
+
+    return (
+        <>
+            <div className="container">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Name</th>
+                            <th scope="col">Description</th>
+                            <th scope="col">Category</th>
+                            <th scope="col">Date</th>
+                            <th scope="col">Place</th>
+                            <th scope="col">Amount</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {tableContent}
+                    </tbody>
+                </table>
+            </div>
+        </>
     );
 }
 
